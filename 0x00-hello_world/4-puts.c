ec5cb45373db28f6508bcee9c35078bbb4f6a7fe
mouadnait <moadnait6@gmail.com>
2023-03-16 11:26:17 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * The puts function in C is used to write a line or string to the output stream (stdout) that is up to, 
  * but does not include, the null character. The puts function also appends a newline character to 
  * the output and returns an integer.
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
