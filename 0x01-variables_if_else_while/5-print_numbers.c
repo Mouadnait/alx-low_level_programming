ba929035ad36598a3b0fa831c429ea0ce7997d8f
mouadnait <moadnait6@gmail.com>
2023-03-17 15:54:17 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
