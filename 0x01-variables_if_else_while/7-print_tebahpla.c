26f70808049715b301ba6263315f881af708858f
mouadnait <moadnait6@gmail.com>
2023-03-17 16:32:51 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	for (char ch = 'z'; ch >= 'a'; ch++)
+	{
+		printf(char);
+	}
+	putchar('\n');
+	return (0);
+}
