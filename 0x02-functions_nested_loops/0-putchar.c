a921a82339b6d1740df899fcc51e5f29b1d87295
mouadnait <moadnait6@gmail.com>
2023-03-21 11:41:50 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
+ * main - check the code
+ *
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- *
- * main - check the code
  * Return: Always 0.
  */
 
