ad65a1d4e00fb6bac6adaaba8560742dc82f2b38
mouadnait <moadnait6@gmail.com>
2023-03-27 15:22:02 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - calculates the length of a string.
+ * @s: pointer to the character array.
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i=0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
