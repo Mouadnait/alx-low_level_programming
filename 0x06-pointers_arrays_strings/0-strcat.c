99fcb15bd6a862705f73da8ad91e2db5715758e5
mouadnait <moadnait6@gmail.com>
2023-03-29 15:33:24 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * Return: pointer to the resulting string dest
+ */
+int str_len(char *str);
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int dest_len = str_len(dest);
+	int src_len = str_len(src);
+
+	for (i = 0; i < src_len; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	return dest;
+}
+
+int str_len(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+	return i;
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: parametor
+ * Return: Always 0.
+ */
+
+int _putchar(int c)
+{
+	putchar(c);
+
+	return (0);
+}
