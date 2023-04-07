bfc1465b2902d9b6c519f73132ef8613ecefe731
mouadnait <moadnait6@gmail.com>
2023-04-06 22:28:39 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,23 @@
+/**
+ * _strncpy - copies a string up to n
+ * @dest: Destination of the pointer to the string
+ * @src: Source of the pointer to the string to copy
+ * @n: Number of bytes to copy.
+ * Return: Pointer to the destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = 0;
+		i++;
+	}
+	return (dest);
+}
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -17,4 +25,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle); 
+
+#endif /* MAIN_H */
