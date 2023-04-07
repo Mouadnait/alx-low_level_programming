3ff28bf7391c351c46ce3fa874edc6c5179c0c37
mouadnait <moadnait6@gmail.com>
2023-04-06 23:19:33 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	argc--;
