661b48724ff353b100c65331eeb30ab07aac7a46
mouadnait <moadnait6@gmail.com>
2023-04-07 20:52:06 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ * str_checker - to check is the string digit or not.
+ * @c: argument vector
+ * Return: 0 on success, 1 on failure
+ */
+int str_checker(char *c)
+{
+	while (*c != '\0')
+	{
+		if (!(*c >= '0' && *c <= '9'))
+			return (0);
+		c++;
+	}
+	return (1);
+}
+
+/**
  * main - program that adds positive numbers.
  * @argc: argument count
  * @argv: argument vectors
@@ -16,11 +32,9 @@ int main(int argc, char *argv[])
 		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]))
+			if (str_checker(argv[i]))
 			{
 				sum += atoi(argv[i]);
-				if (i == argc - 1)
-					printf("%d\n", sum);
 			}
 			else
 			{
@@ -28,6 +42,7 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
+		printf("%d\n", sum);
 	}
 	return (0);
 }
