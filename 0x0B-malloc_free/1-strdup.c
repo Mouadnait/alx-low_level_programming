1a9058800ae05a0d2c50db47e8706e8007d9d8a6
mouadnait <moadnait6@gmail.com>
2023-04-11 15:58:58 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
+/**
+ * _strdup - copies a string into a new buffer
+ * @str: the string to copy
+ * Return: pointer to a new buffer
+ */
 char *_strdup(char *str)
 {
 	char *str_arr;
 	unsigned int i;
 
-	str_arr = (char *) malloc(strlen(str) * sizeof(char));
+	str_arr = (char *) malloc((strlen(str) + 1) * sizeof(char));
 
-	if (str == NULL)
+	if (!str || !str_arr)
 		return (NULL);
 	i = 0;
-	while (i < strlen(str))
+	while (i < strlen(str) + 1)
 	{
 		str_arr[i] = str[i];
 		i++;
