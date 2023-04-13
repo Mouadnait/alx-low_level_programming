87e235ba6989a95174603148e68af53e15c3709d
mouadnait <moadnait6@gmail.com>
2023-04-13 15:44:10 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * malloc_checked - allocates memory using malloc
+  * @b: size of buffer
+  * Return: pointer to buffer
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b));
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
