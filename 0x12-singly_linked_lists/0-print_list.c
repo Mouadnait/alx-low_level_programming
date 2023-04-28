2e655267eef003e55072dfc97a203605d805847c
mouadnait <moadnait6@gmail.com>
2023-04-28 16:35:30 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * print_list - prints all elements of a list_t list
+  * @h: singly linked list to print
+  * Return: number of nodes in the list
+  */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *copy_head;
+
+
+
+	copy_head = h;
+	while (copy_head != NULL)
+	{
+		printf("[%d] %s\n", copy_head->len, copy_head->str);
+		copy_head = copy_head->next;
+		count++;
+	}
+	return (count);
+}
