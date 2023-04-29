14ead2a47d1b2de4e0e56f09e0a9bb4b755cd974
Mouad Nait si <91064574+Mouadnait@users.noreply.github.com>
2023-04-29 00:08:48 +0000

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,20 @@
 #include "lists.h"
 
-
+/**
+  * list_len - finds the number of nodes in a list
+  * @h: singly linked list
+  * Return: number of elements in a linked list
+  */
 size_t list_len(const list_t *h)
 {
 	int count = 0;
 	const list_t *copy_head;
 
 	copy_head = h;
-	while (copy_head->next != NULL)
+	while (copy_head != NULL)
 	{
 		copy_head = copy_head->next;
-		count += 2;
+		count++;
 	}
 	return (count);
 }
