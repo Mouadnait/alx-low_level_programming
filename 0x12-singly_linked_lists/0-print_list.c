18da869fdad6f82a83aa814c9f952cbf59411802
Mouad Nait si <91064574+Mouadnait@users.noreply.github.com>
2023-04-29 00:28:07 +0000

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,17 @@
   */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[0] (nil)");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s", h->len, h->str);
 		h = h->next;
 		count++;
+		printf("\n");
 	}
 	return (count);
 }
