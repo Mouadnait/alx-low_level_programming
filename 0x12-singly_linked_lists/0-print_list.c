ef62bbd30eaf6af6e79851aa5fd030f3e190a0c8
Mouad Nait si <91064574+Mouadnait@users.noreply.github.com>
2023-04-29 00:05:03 +0000

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,13 @@ size_t print_list(const list_t *h)
 	int count = 0;
 	const list_t *copy_head;
 
-
-
 	copy_head = h;
 	while (copy_head != NULL)
 	{
-		printf("[%d] %s\n", copy_head->len, copy_head->str);
+		if (!copy_head->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", copy_head->len, copy_head->str);
 		copy_head = copy_head->next;
 		count++;
 	}
