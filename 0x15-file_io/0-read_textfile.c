ce78ef68fda8fdf108194422113357a8915e77f9
mouadnait <moadnait6@gmail.com>
2023-05-09 23:22:52 +0100

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,31 +9,33 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, err, rd;
+	int fd;
+	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
-	fd = err = rd = 0;
-	if (!filename || !letters)
+	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters + 1);
-	if (!buffer)
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
 		return (0);
 
-	rd = read(fd, buffer, letters);
-	if (rd < 0)
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
-	buffer[letters] = '\0';
-	err = write(STDOUT_FILENO, buffer, rd);
-	if (err <= 0)
+
+	buffer[bytes_read] = '\0';
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		free(buffer);
 		close(fd);
@@ -42,5 +44,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	free(buffer);
 	close(fd);
-	return (rd);
+
+	return (bytes_written);
 }
