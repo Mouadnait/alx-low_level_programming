c3846ed21ae0be695bcf116e951853699c1fffec
mouadnait <moadnait6@gmail.com>
2023-12-14 02:50:05 +0100

    C - Search Algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* _search_algos_ */
